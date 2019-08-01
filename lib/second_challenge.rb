5d8cbcb440728f6831dcdea88cf167c47679458e
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-08-01 02:08:35 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
   
